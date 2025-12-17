7561a9fad1f90aadf43a339bddb166152ce69bfd
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-12-18 00:16:17 +0300

    6th task in the binary trees project

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,7 +3,7 @@
 /**
 	* binary_tree_is_leaf - checks if a node is a leaf
 	* @node: pointer to the node to check
-    *
+	*
 	* Return: 1 if node is a leaf, 0 otherwise or if node is NULL
 	*/
 int binary_tree_is_leaf(const binary_tree_t *node)
