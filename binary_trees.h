c6b6e37c6282a3a69f7d731f7dc6eeac4b64edbe
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-12-18 00:12:35 +0300

    5th task in binary trees project

--- binary_trees.h
+++ binary_trees.h
@@ -4,13 +4,13 @@
 #include <stddef.h>
 
 /**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child
- * @right: Pointer to the right child
- */
+	* struct binary_tree_s - Binary tree node
+	*
+	* @n: Integer stored in the node
+	* @parent: Pointer to the parent node
+	* @left: Pointer to the left child
+	* @right: Pointer to the right child
+	*/
 typedef struct binary_tree_s
 {
 	int n;
@@ -24,6 +24,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
 
 #endif
 
