45e5c950ba41518e90d037e7362b2e2419843605
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-12-17 17:00:55 +0300

    1st task

--- binary_trees.h
+++ binary_trees.h
@@ -3,13 +3,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * struct binary_tree_s - Basic binary tree node
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ *
+ * Description: This structure defines a node of a
+ * binary tree, containing an integer value and
+ * pointers to parent, left, and right child nodes.
+ */
 typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 } binary_tree_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
