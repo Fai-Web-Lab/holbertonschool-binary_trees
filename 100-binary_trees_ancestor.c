df11f1dd6aa8d02a2e95e55cb9fbc046cb505851
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-12-19 21:35:49 +0300

    1st advanced in binary trees

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - find the lowest common ancestor of 2 nodes
+ * @first: pointer to the 1st node
+ * @second: pointer to the 2nd node
+ * 
+ * Return: pointer to the lowest common ancestor, or NULL if none
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+    const binary_tree_t *a, *b;
+
+    if (first == NULL || second == NULL)
+    return (NULL);
+
+    a = first;
+    while (a)
+    {
+        b = second;
+        while (b)
+        {
+            if (a == b)
+            return ((binary_tree_t *)a);
+            b = b->parent;
+        }
+        a = a->parent;
+    }
+
+    return (NULL);
+}
